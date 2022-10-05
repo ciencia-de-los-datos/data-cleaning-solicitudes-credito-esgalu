cf934bfda78bef4757947a4bab6ebc4951eb0f43
esgalu <58479403+esgalu@users.noreply.github.com>
2022-10-04 20:22:31 -0500

    feat: (@esgalu) Update preguntas.py - solved

--- pregunta.py
+++ pregunta.py
@@ -6,15 +6,46 @@ Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizad
 correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
-import pandas as pd
+
+def normalize_date(x):
+    try:
+        return datetime.strptime(x, '%d/%m/%Y')
+    except ValueError:
+        return datetime.strptime(x, '%Y/%m/%d')
 
 
 def clean_data():
+    df = pd.read_csv(
+        "solicitudes_credito.csv",
+        sep=";",
+        usecols=[
+            'sexo', 'tipo_de_emprendimiento', 'idea_negocio',
+            'barrio', 'estrato', 'comuna_ciudadano',
+            'fecha_de_beneficio', 'monto_del_credito', 'línea_credito']
+    )
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df = df.dropna()
 
-    #
-    # Inserte su código aquí
-    #
+    list_col = ['sexo', 'tipo_de_emprendimiento', 'idea_negocio',
+                'barrio', 'línea_credito']
+    for col in list_col:
+        df[col] = df[col].str.lower()
+
+    list_char = [' ', '-']
+    for col in ['idea_negocio', 'línea_credito', 'barrio']:
+        for char in list_char:
+            df[col] = df[col].str.replace(char, '_')
+
+    list_char_m = ['$', ',', '.00']
+    col = 'monto_del_credito'
+    for char in list_char_m:
+        df[col] = df[col].apply(lambda s: s.replace(char, ''))
+
+    df.monto_del_credito = df.monto_del_credito.str.strip()
+
+    df['fecha_de_beneficio'] = df['fecha_de_beneficio'].apply(
+        lambda x: normalize_date(x))
+
+    df.drop_duplicates(inplace=True)
 
     return df
